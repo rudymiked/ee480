90d4d16d2f35408fc3feb1a2bb0c0df5dce5a5e3
Mike Rudy <rudymiked@gmail.com>
2014-04-22 10:41:00 -0400

    all ISA usable

--- asm/assembler.rb
+++ asm/assembler.rb
@@ -30,7 +30,7 @@ class Assembler
     @op_ld  = "001110"  # LOAD TO RAM
     @op_st  = "001111"  # STORE TO RAM
     @op_in  = "010000"  # INPUT WORD TO RAM
-    @op_asl = "010001"  # OUTPUT WORD FROM RAM
+    @op_out = "010001"  # OUTPUT WORD FROM RAM
     @op_msk = "010010"  # HVPI
 
   end
@@ -42,28 +42,44 @@ class Assembler
     file.each { |l|
       l = l.split
       case l[0]
-      when "and"
-        file_out.puts "#{@op_and} #{l[1]} #{l[2]}"
       when "add"
-        file_out.puts "#{@op_add} #{l[1]} #{l[2]}"
-      when "sub" 
-        file_out.puts "#{@op_sub} #{l[1]} #{l[2]}"
+        file_out.puts "#{@op_add} #{l[1]}"
+      when "sub"
+        file_out.puts "#{@op_sub} #{l[1]}"
+      when "or"
+        file_out.puts "#{@op_or} #{l[1]}"
+      when "and"
+        file_out.puts "#{@op_and} #{l[1]}" #{l[2]}"
+      when "not" 
+        file_out.puts "#{@op_not} #{l[1]}"
+      when "mult" 
+        file_out.puts "#{@op_mul} #{l[1]}"
+      when "div" 
+        file_out.puts "#{@op_div} #{l[1]}"
+      when "asr" 
+        file_out.puts "#{@op_asr} #{l[1]}"
+      when "asl" 
+        file_out.puts "#{@op_asl} #{l[1]}"
+      when "bra" 
+        file_out.puts "#{@op_bra} #{l[1]}"
       when "jmp" 
-        file_out.puts "#{@op_jmp} #{l[1]} #{l[2]}"
-      when "mov" 
-        file_out.puts "#{@op_mov} #{l[1]} #{l[2]}"
-      when "bne" 
-        file_out.puts "#{@op_bne} #{l[1]} #{l[2]} #{l[3]}"
-      when "beq" 
-        file_out.puts "#{@op_beq} #{l[1]} #{l[2]} #{l[3]}"
+        file_out.puts "#{@op_jmp}"
+      when "jsr" 
+        file_out.puts "#{@op_jsr} #{l[1]}"
+      when "rts" 
+        file_out.puts "#{@op_rts} #{l[1]}"
+      when "isr"
+        file_out.puts "#{@op_isr} #{l[1]}"
+      when "ld" 
+        file_out.puts "#{@op_ld} #{l[1]}"
+      when "st" 
+        file_out.puts "#{@op_st} #{l[1]}"
       when "in" 
         file_out.puts "#{@op_in} #{l[1]}"
       when "out" 
         file_out.puts "#{@op_out} #{l[1]}"
-      when "asr"
-        file_out.puts "#{@op_asr} #{l[1]}"
-      when "asl"
-        file_out.puts "#{@op_asl} #{l[1]}"
+      when "msk"
+        file_out.puts "#{@op_msk} #{l[1]}"
       when "\#"
         next
       end
