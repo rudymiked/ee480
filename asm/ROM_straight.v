10558c838892ff99d224ddbf2474d7b5f60868fa
Mike Rudy <rudymiked@gmail.com>
2014-04-25 11:36:30 -0400

    added more

--- asm/ROM_straight.v
+++ asm/ROM_straight.v
@@ -4,7 +4,7 @@
 ## University of Kentucky
 ## EE 480 Spring 2014
 ##
-## Module: ROM_ROM_straight.v 
+## Module: ROM_straight.v 
 ## Dependencies: N/A
 ##
 ## Description: initial RAM module for ROM_straight.v program
@@ -12,3 +12,16 @@
 ## Last Modified: Michael - 4/24/2014
 ##
 ##################################################################
+module ROM_straight.v(ce, reg_in, addr, rw, clk, clr, reg_out);
+
+  parameter addr_width = 8, data_width = 8;
+  input rw, clr, clk, ce;
+  input [(data_width-1):0] reg_in;
+  input [(addr_width-1):0] addr;
+
+  output [(data_width-1):0] reg_out;
+  reg [(data_width-1):0] reg_out;
+  integer i;
+  reg [(data_width-1):0] mem[((2**addr_width)-1):0];
+
+end
--- asm/rom_create.rb
+++ asm/rom_create.rb
@@ -20,7 +20,8 @@ class RomCreate
   def initialize
     @file_name 
     @header 
-             
+    @module
+    @memory
   end
 
   def parse
@@ -53,13 +54,33 @@ class RomCreate
 ##
 ##################################################################
     eos
+
   end
 
 
   def build
     rom_file = File.open(@file_name, "w")  
     rom_file.puts @header
+    
 
+    @module = <<-eos
+module #{@file_name}(ce, reg_in, addr, rw, clk, clr, reg_out);
+
+  parameter addr_width = 8, data_width = 8;
+  input rw, clr, clk, ce;
+  input [(data_width-1):0] reg_in;
+  input [(addr_width-1):0] addr;
+
+  output [(data_width-1):0] reg_out;
+  reg [(data_width-1):0] reg_out;
+  integer i;
+  reg [(data_width-1):0] mem[((2**addr_width)-1):0];
+
+    eos
+
+    rom_file.puts @module
+    rom_file.puts "end" 
+    rom_file.close
   end
 
 
